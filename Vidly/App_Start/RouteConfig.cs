d1bad25c7191dddf1cc7cbe0491f987e46a3a01d
Ayush Bhargava <ayushb013@gmail.com>
2019-07-19 21:19:52 +0530

    Views Added

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,6 +12,9 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+            //routes.MapRoute("MoviesByReleaseDate", "movies/released/{year}/{month}", new { controller = "Movies", action = "ByReleaseDate"}, new { year = @"\d{4}", month = @"\d{2}"});
+            //routes.MapRoute("MoviesByReleaseDate", "movies/released/{year}/{month}", new { controller = "Movies", action = "ByReleaseDate" }, new { year = @"2015|2016", month = @"\d{2}" });
 
             routes.MapRoute(
                 name: "Default",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,7 +14,53 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             Movie movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"},
+                new Customer {Name = "Customer 3"},
+                new Customer {Name = "Customer 4"},
+                new Customer {Name = "Customer 5"},
+                new Customer {Name = "Customer 6"},
+            };
+
+            RandomMovieViewModel viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        [Route("movies/released/{year:regex(^\\d{4}$)}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+
+            return Content(year + "/" + month);
         }
     }
 }
